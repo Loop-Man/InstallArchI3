9cbe19f046cd18f0fbad8dd023a1205cd61f76a6
Loop-Man <60947094+Loop-Man@users.noreply.github.com>
2021-05-04 00:08:30 +0200

    Update InstallZSH.sh

--- InstallZSH.sh
+++ InstallZSH.sh
@@ -20,8 +20,7 @@ sudo pacman -S --noconfirm --needed bat
 
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
 sed -i 's/\~\/.oh-my-zsh\}$/\~\/.zsh\/.oh-my-zsh\}/g' install.sh
-sh install.sh &>/dev/null &
-sleep 20
+sh install.sh
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.zsh/.oh-my-zsh/themes/powerlevel10k
 sed -i 's/robbyrussell/powerlevel10k\/powerlevel10k/1' .zshrc
 rm -rf .zshrc.tmp
